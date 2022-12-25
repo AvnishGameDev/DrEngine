ae05b922e5300547dfd311dd7346f36f9beacde9
Avnish Kirnalli <avnishkirnalli@gmail.com>
2022-12-25 15:46:12 +0530

    Added python script to SetupProject

--- SetupDependencies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Copyright Avnish Kirnalli 2022.
-echo Init Submodules
-git submodule init
-echo Updating Submodules
-git submodule update
-echo Updated Submodules
-echo Run GenerateProjects.bat to generate Solution
-PAUSE
